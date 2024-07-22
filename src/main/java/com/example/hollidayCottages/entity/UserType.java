ab58e01d9ee7b5a4e4314e44b628f7a37041aca4
kavunidze <kavunidze@protonmail.com>
2024-07-22 19:08:43 +0300

    Update settings

---
+++ src/main/java/com/example/hollidayCottages/entity/UserType.java
@@ -0,0 +1,15 @@
+package com.example.hollidayCottages.entity;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@AllArgsConstructor
+@NoArgsConstructor
+@Getter
+@Setter
+public class UserType {
+    private int id;
+    private String name;
+}
